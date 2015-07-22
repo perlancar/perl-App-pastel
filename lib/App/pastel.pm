734b38028b66f6b4952202afd6117dbf76de553d
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-22 15:27:27 +0700

    Initial commit

---
+++ lib/App/pastel.pm
@@ -0,0 +1,16 @@
+package App::pastel;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Pastelize colors for terminal
+
+=head1 DESCRIPTION
+
+This distribution contains the script L<pastel>.
+
+
+=head1 SEE ALSO
+
+L<pastel>
